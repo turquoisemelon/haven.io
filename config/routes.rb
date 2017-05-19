6b1af309ee4d5b095ccab9b245c813e64871d7f9
jerry <jerry.h.zhan@gmail.com>
2017-05-19 19:50:29 -0400

    added a link to user_profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sessions#new'
 
-  # post '/reports/new' => 'reports#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   # These routes will be for signup. The first renders a form in the browse, the second will
   # receive the form and create a user in our database using the data given to us by the user.
   resources :users, except: [:index, :show, :destroy] do
-    # root to: 'users#index',  as: 'user'
     resources :reports, only: [:index, :show, :create, :new]
   end
   get '/signup' => 'users#new'
