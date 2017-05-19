c2082bd52e0391ecb16c106e52ca821aa2eb4782
jerry <jerry.h.zhan@gmail.com>
2017-05-19 17:52:55 -0400

    removed scaffold css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518221450) do
+ActiveRecord::Schema.define(version: 20170519163512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20170518221450) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "profession"
+    t.string "marital_status"
+    t.boolean "hiv"
+    t.boolean "veteran"
+    t.boolean "immigrant"
   end
 
 end
