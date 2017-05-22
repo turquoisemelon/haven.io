127b676ba8261dd124bd49ffc152ab51e76e3a03
Andrew J Royce <andrewjroyce@gmail.com>
2017-05-22 15:55:21 -0400

    modified history and reports, added accuity and vulnerability dbs

--- db/migrate/20170520172057_create_histories.rb
+++ db/migrate/20170520172057_create_histories.rb
@@ -1,29 +1,23 @@
 class CreateHistories < ActiveRecord::Migration[5.1]
   def change
     create_table :histories do |t|
-      t.belongs_to :user
+        t.belongs_to :user
       t.boolean :stolen 
-      t.boolean :friends 
-      t.boolean :clothes 
       t.integer :eaten 
-      t.boolean :trash 
-      t.boolean :new_conditions
       t.boolean :health_condition  
-      t.string :health_condition_note
+        t.string :health_condition_note
       t.boolean :appointments 
-      t.boolean :depressed 
-      t.boolean :mental_health 
-      t.string :mental_health_note
+      t.boolean :mental_health
+        t.string :mental_health_note
       t.boolean :new_habit 
-      t.string :new_habit_note
-      t.boolean :AA_NA 
+        t.string :new_habit_note
       t.boolean :social_circle 
       t.boolean :communicate_needs 
       t.integer :cooperate 
-      t.datetime :created_at 
       t.integer :housing_situation
-      t.timestamps null: false
-      t.text :comments
+        t.text :comments
+        t.datetime :created_at 
+        t.timestamps null: false
     end
   end
 end
---
+++ db/migrate/20170522194924_accuity.rb
@@ -0,0 +1,19 @@
+class Accuity < ActiveRecord::Migration[5.1]
+  def change
+    create_table :accuity do |t|
+t.boolean :treatment
+t.boolean :crisis
+t.boolean :medication
+t.boolean :housing
+t.boolean :basic_needs
+t.boolean :income
+t.boolean :substances
+t.boolean :emergency
+t.boolean :weeks_homeless
+t.boolean :moods
+t.user_id :user_id
+t.datetime :created_at
+t.timestamps null: false
+   end
+  end
+end
