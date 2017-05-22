e411cf80d9d15cdaf9d748689d991a565be1bd4a
Meltem <melltemkilic@gmail.com>
2017-05-22 16:52:05 -0400

    current_user and edit method added to home_controller to achieve redirecting to the users/:id/edit url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
