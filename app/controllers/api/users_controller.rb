14f0b0618796a19a9d8aa66c08ebf1d556da4669
Meltem <melltemkilic@gmail.com>
2017-05-23 20:39:18 -0400

    name method added to the api/users controller to get a list of users with their names and genders

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::UsersController < Api::APIController
   before_action :authorize_admin
 
+  def name
+    @users = User.select("name, gender").where("admin = false")
+    # @users['id'] = User.select(:gender).where("admin = false")
+    render json: @users
+  end
+
   def find
     @user = User.find(params[:id])
     render json: @user
