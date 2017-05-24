202223d1ad12a17722144b6c86a414045c245241
Andrew J Royce <andrewjroyce@gmail.com>
2017-05-24 19:03:50 -0400

    fixed accuity

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -3,14 +3,14 @@ class Api::ReportsController < Api::UsersController
 
   def accuity
     @accuity = {}
-      @accuity['Treatment Participation'] = Report.where("user_id = user_id AND meetings = true").count
-      @accuity['Crises Incidents'] = Report.where("user_id = user_id AND arrested = true").count
-      @accuity['Medicine Compliance'] = Report.where("user_id = user_id AND medicated = true").count
-      @accuity['Housing'] = Report.where("user_id = user_id AND indoors = true").count
-      @accuity['Meets basic'] = Report.where("user_id = user_id AND bathed = true").count
-      @accuity['Substance Abuse'] = Report.where("user_id = user_id AND drugs = true").count
-      @accuity['Danger to self'] = Report.where("user_id = user_id AND fights = true").count
-      @accuity['Other Problems'] = Report.where("user_id = user_id AND ems = true").count
+      @accuity['Treatment Participation'] = Report.where("user_id = ? AND meetings = true", params[:id]).count
+      @accuity['Crises Incidents'] = Report.where("user_id = ? AND arrested = true", params[:id]).count
+      @accuity['Medicine Compliance'] = Report.where("user_id = ? AND medicated = true", params[:id]).count
+      @accuity['Housing'] = Report.where("user_id = ? AND indoors = true", params[:id]).count
+      @accuity['Meets basic'] = Report.where("user_id = ? AND bathed = true", params[:id]).count
+      @accuity['Substance Abuse'] = Report.where("user_id = ? AND drugs = true", params[:id]).count
+      @accuity['Danger to self'] = Report.where("user_id = ? AND fights = true", params[:id]).count
+      @accuity['Other Problems'] = Report.where("user_id = ? AND ems = true", params[:id]).count
      # missing  weeks_homeless: 0,
      # missing  sentiment: 5,
       render json: @accuity
