d3cf8287fa7e03bd2d5ebe0339765c439fe801ea
Meltem <melltemkilic@gmail.com>
2017-05-24 11:18:11 -0400

    id param added to the select query

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,7 @@ class Api::UsersController < Api::APIController
   before_action :authorize_admin
 
   def name
-    @users = User.select("name, gender").where("admin = false")
-    # @users['id'] = User.select(:gender).where("admin = false")
+    @users = User.select("id, name, gender").where("admin = false")
     render json: @users
   end
 
