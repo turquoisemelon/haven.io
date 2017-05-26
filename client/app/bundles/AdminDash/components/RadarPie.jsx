e19aeb192fb71963fdbfcb059d4155eec1cb95b0
Meltem <melltemkilic@gmail.com>
2017-05-26 17:41:03 -0400

    data labels of the chart changed. chart resized

--- client/app/bundles/AdminDash/components/RadarPie.jsx
+++ client/app/bundles/AdminDash/components/RadarPie.jsx
@@ -5,7 +5,7 @@ export default class RadarPie extends React.Component{
   render(){
     return(
       <div className="radar-pie">
-        <RadarChart cx={165} cy={175} outerRadius={100} width={400} height={350} data={this.props.data}>
+        <RadarChart cx={172} cy={175} outerRadius={120} width={400} height={350} data={this.props.data}>
           <Radar name="Radar" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
           <PolarGrid />
           <PolarAngleAxis dataKey="subject" />
